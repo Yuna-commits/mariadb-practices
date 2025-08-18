09aa29d23db6bce54530f41c97cce21809f1f41d
Yuna-commits <ksm13563661@gmail.com>
2025-08-18 17:58:55 +0900

    practice02 solve

--- sql-practices/practice02.sql
+++ sql-practices/practice02.sql
@@ -3,20 +3,50 @@
 -- 문제 1. 
 -- 최고임금(salary)과  최저임금을 "최고임금", "최저임금" 프로젝션 타이틀로 함께 출력하세요.
 -- 그리고 두 임금의 차이는 얼마인가요? "최고임금 – 최저임금" 이란 타이틀로 출력하세요.
+SELECT 
+	MAX(salary) AS '최고임금',
+    MIN(salary) AS '최저임금', 
+    MAX(salary)-MIN(salary) AS '최고-최저'
+FROM salaries;
 
 -- 문제2.
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력하세요
 -- 예) 2014년 07월 10일
+SELECT 
+    DATE_FORMAT(MAX(hire_date), '%Y년 %m월 %d일')
+FROM
+    employees;
 
 -- 문제3.
 -- 가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력하세요.
 -- 예) 2014년 07월 10일
+SELECT 
+    DATE_FORMAT(MIN(hire_date), '%Y년 %m월 %d일')
+FROM
+    employees;
 
 -- 문제4.
 -- 현재, 이 회사의 평균 연봉은 얼마입니까?
+SELECT 
+    AVG(salary)
+FROM
+    salaries
+WHERE
+    to_date = '9999-01-01';
 
 -- 문제5.
--- 현재, 이 회사의 최고/최저 연봉은 각각 얼마입니까?
+-- 현재, 이 회사의 최고, 최저 연봉은 각각 얼마입니까?
+SELECT 
+    MAX(salary), MIN(salary)
+FROM
+    salaries
+WHERE
+    to_date = '9999-01-01';
 
 -- 문제6.
 -- 현재, 근무중인 사원 중 나이가 제일 어린 사원과 제일 많은 사원의 나이를 각각 출력하세요.
+SELECT 
+    DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(MIN(birth_date), '%Y') as '최고령자',
+    DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(MAX(birth_date), '%Y') as '최연소'
+FROM
+    employees;
