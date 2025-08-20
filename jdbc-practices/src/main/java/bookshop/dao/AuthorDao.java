2eee27807761f94edefed04dcb095bca99beecc1
Yuna-commits <ksm13563661@gmail.com>
2025-08-20 14:54:31 +0900

    author dao 수정

--- jdbc-practices/src/main/java/bookshop/dao/AuthorDao.java
+++ jdbc-practices/src/main/java/bookshop/dao/AuthorDao.java
@@ -38,7 +38,9 @@ public class AuthorDao {
 			result = pstmt1.executeUpdate();
 
 			ResultSet rs = pstmt2.executeQuery();
-			// 방금 insert된 author_id의 값을 JAVA는 모름, 직접 설정해야 함
+			// 방금 insert된 id의 값을 JAVA는 모름, 직접 설정해야 함
+			// author의 id와 BookVo의 author_id를 연결해야 함
+			// BookShopApp의 bookVo.setAuthorId(authorVo.getId())에 사용하기 위해 필수!
 			vo.setId(rs.next() ? rs.getLong(1) : null);
 
 			rs.close();
